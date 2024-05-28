ff5754a8ffbf8b0395b4cf05ee436559432e0b60
siwolee <siwolee@student.42seoul.kr>
2024-05-28 20:26:19 +0900

    SDAF

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,7 +1,18 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
+#include <stdexcept>
 
 template <typename T>
-easyfind(T& container, int& target) {}
+int easyfind(T& container, int& target) throw(std::out_of_range) {
+  std::iterator<T> iter = container.begin();
+  try {
+    std::cout << iter << std::endl;
+    while (iter != container.end()) {
+      if (*iter == target) return iter;
+    }
+    iter++;
+  }
+  throw std::out_of_range("no occurance found");
+}
 
 #endif
