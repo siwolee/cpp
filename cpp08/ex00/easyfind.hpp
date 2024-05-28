1500ab88d45c71a7fbdfc2e4562d50d6a3a1547f
haecho <fimicat@gmail.com>
2024-05-28 21:51:14 +0900

    08: ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -3,16 +3,17 @@
 #include <stdexcept>
 
 template <typename T>
-int easyfind(T& container, int& target) throw(std::out_of_range) {
-  std::iterator<T> iter = container.begin();
-  try {
-    std::cout << iter << std::endl;
-    while (iter != container.end()) {
-      if (*iter == target) return iter;
+int easyfind(T &container, int &target) throw(std::out_of_range) {
+    std::iterator<T> iter = container.begin();
+    try {
+        std::cout << iter << std::endl;
+        while (iter != container.end()) {
+            if (*iter == target)
+                return iter;
+        }
+        iter++;
     }
-    iter++;
-  }
-  throw std::out_of_range("no occurance found");
+    throw std::out_of_range("no occurance found");
 }
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 // sequence containers : iteratable
-#include <array>         //static, contiguous
-#include <deque>         // double-ended queue
-#include <forward_list>  //single ended list (->)
-#include <list>          //doubly ended list (<->)
-#include <vector>        //dynamic, contiguous
+#include <array>        //static, contiguous
+#include <deque>        // double-ended queue
+#include <forward_list> //single ended list (->)
+#include <list>         //doubly ended list (<->)
+#include <vector>       //dynamic, contiguous
 // associative containers: implement sorted data structures
 // set, map, multiset, multimap.
 
 #include "easyfind.hpp"
 
 int main() {
-  // empty test
-  {
-    std::array<int, 0> empty_array;
-    // std::deque<int> empty_deque;
-    // std::forward_list<int> empty_forward_list;
-    // std::list<int> empty_list;
-    // std::vector<int> empty_vector;
-    int target = 1;
+    // empty test
+    {
+        std::array<int, 0> empty_array;
+        // std::deque<int> empty_deque;
+        // std::forward_list<int> empty_forward_list;
+        // std::list<int> empty_list;
+        // std::vector<int> empty_vector;
+        int target = 1;
 
-    try {
-      std::cout << "Array empty :" << easyfind(empty_array, target)
-                << std::endl;
-    } catch (std::exception& e) {
-      std::cerr << e.what() << std::endl;
+        try {
+            std::cout << "Array empty :" << easyfind(empty_array, target)
+                      << std::endl;
+        } catch (std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
     }
-  }
 }
