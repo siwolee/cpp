576fc9afee5bf2e31d0993d59de91b24c06ee887
siwolee <siwolee@student.42seoul.kr>
2024-06-24 23:08:04 +0900

    09: ford johnson 씨 맞으십니까?

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -24,32 +24,57 @@ void _swap(std::vector<T>& arr, size_t front, size_t back, size_t size) {
  */
 template <typename T>
 void _insert(std::vector<T>& arr, size_t curr, size_t target, size_t size) {
-  T temp[size];
-  curr -= size;
-  target -= size;
+  std::vector<T> temp_a(size);
+  std::vector<T> temp_b(size);
   size_t i = 0;
-  for (size_t idx = target; idx <= curr + size; idx++) {
-    temp[i] = arr[idx];
-    arr[idx] = arr[curr + i];
-    i = i > size - 1 ? i + 1 : 0;
+  for (size_t idx = target - size + 1; idx <= target; idx++) {
+    temp_a[i] = arr[idx];
+    arr[idx] = arr[curr - size + 1 + i];
+    if (i < size - 1) {
+      i += 1;
+    } else {
+      i = 0;
+    }
+  }
+  for (size_t idx = target + 1; idx <= curr; idx++) {
+    temp_b[i] = arr[idx];
+    arr[idx] = temp_a[i];
+    temp_a[i] = temp_b[i];
+    if (i < size - 1) {
+      i++;
+    } else {
+      i = 0;
+    }
   }
 }
 
 template <typename T>
 size_t _divideConquer(std::vector<T>& arr, std::vector<size_t> main_seq,
                       size_t idx) {
-  size_t standard = idx / 2;
-  size_t pos = standard;
-  while (standard > 1) {
-    standard /= 2;
-    if (arr[main_seq[idx]] < arr[main_seq[pos]]) {
-      pos += standard;
-    } else if (arr[main_seq[idx]] > arr[main_seq[pos]]) {
-      pos -= standard;
-    } else
-      break;
+  size_t left = 0;
+  size_t right = idx - 1;
+  size_t mid = (left + right) / 2;
+  T res = 0;
+  while (left < right) {
+    mid = (left + right) / 2;
+    res = arr[main_seq[idx]] - arr[main_seq[mid]];
+    if (res > 0) {  // mid < idx
+      left = mid + 1;
+    } else if (res < 0) {  // mid > idx
+      right = mid;
+    } else {
+      return mid + 1;
+    }
   }
-  return pos;
+  if (arr[main_seq[idx]] > arr[main_seq[right]]) {
+    return idx;
+  } else if (arr[main_seq[idx]] > arr[main_seq[mid]]) {
+    return right;
+  } else if (arr[main_seq[idx]] > arr[main_seq[left]]) {
+    return mid;
+  } else {
+    return left;
+  }
 }
 
 template <typename T>
@@ -63,9 +88,9 @@ void merge_insert_sort(std::vector<T>& arr, size_t rank) {
   }
   // get main(winner) sequence
   // swap the elements - winner goes for last
-  for (size_t front = rank - 1; front < arr.size(); front += 2 * rank) {
+  for (size_t front = rank - 1; front + rank < arr.size(); front += 2 * rank) {
     size_t back = front + rank;
-    if (arr[back] > arr[front]) {
+    if (arr[back] < arr[front]) {
       _swap(arr, front, back, rank);
     }
     main_seq.push_back(front);
@@ -73,7 +98,7 @@ void merge_insert_sort(std::vector<T>& arr, size_t rank) {
   }
   // recv for winner sequence
   if (main_seq.size() > 2) {
-    merge_insert_sort(main_seq, rank * 2);
+    merge_insert_sort(arr, rank * 2);
   }
 
   // add the last element if the array is odd
@@ -82,9 +107,9 @@ void merge_insert_sort(std::vector<T>& arr, size_t rank) {
   }
 
   // merge the two sequences by divide / insert
-  for (size_t idx = 0; idx < main_seq.size(); idx++) {
+  for (size_t idx = 2; idx < main_seq.size(); idx++) {
     size_t pos = _divideConquer(arr, main_seq, idx);
-    _insert(arr, main_seq[idx], main_seq[pos], rank);
+    if (pos < idx) _insert(arr, main_seq[idx], main_seq[pos], rank);
   }
 }
 // Explicit template instantiation
